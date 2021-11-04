a0146dc89bad0294ddba04dd5afb8323c61305f2
cwilly89 <88181324+cwilly89@users.noreply.github.com>
2021-11-04 00:58:48 -0700

    included github and netlify hyperlinks

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather defaultCity="Seattle" />
+
+      <footer>
+        This project was coded by Channell Mascaro and is
+        <a
+          href="https://github.com/cwilly89/react-weather-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub {""}
+        </a>
+        and{" "}
+        <a
+          href="https://reverent-swanson-1aaafe.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify.
+        </a>
+      </footer>
     </div>
   );
 }
